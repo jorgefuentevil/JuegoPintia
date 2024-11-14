56d6fc3d7491bd9f5bc1d56800ba54d1c34efd4c
jorgefuentevil <jorge.fuente.villanueva@estudiantes.uva.es>
2024-11-14 20:14:13 +0100

    HistoryManager2.0

--- Assets/Scripts/HistoryManager.cs
+++ Assets/Scripts/HistoryManager.cs
@@ -2,18 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization;
+using TMPro;
 
 
 public class HistoryManager : MonoBehaviour
 {
     [SerializeField]
     private LocalizedAsset<TextAsset> jsonHistoria;
+    [SerializeField] private TextMeshProUGUI nombrePersonajeText;
+    [SerializeField] private TextMeshProUGUI usuarioText;
+    [SerializeField] private TextMeshProUGUI preguntaText;
+        [SerializeField] private TextMeshProUGUI anosText;
+
+    [SerializeField] private TextMeshProUGUI respuestaIText;
+    [SerializeField] private TextMeshProUGUI respuestaDText;
+    [SerializeField] private int nPreguntas;
 
     private HistoryJsonRoot parsedHistorias;
+    [SerializeField] private int anosThreshold=5;
+    private int nAnosIni=10;
 
+    
     public void Start(){
         parsedHistorias = JsonUtility.FromJson<HistoryJsonRoot>(jsonHistoria.LoadAsset().text);
+        Dictionary<string, Sprite> retratosPersonajes = new Dictionary<string, Sprite>();
+        //Crear la lista de decisiones al azar
+        //Cargar imagenes en diccionario
+
+        //cargar imagenes a distintas z o activar solo la q sale
+
+        //Cargar nombre de usuario
         
+    }
+    public void SetLevelData(int level_index)
+    {
+        if (level_index < 0 || level_index >= nPreguntas)
+        {
+            Debug.Log("Error Level Index out of bounds " + level_index + " of " + nPreguntas);
+            return;
+        }
+        Decision decision = parsedHistorias.decisions[level_index];
+        nombrePersonajeText.SetText(decision.imagen);
+        preguntaText.SetText(decision.desc);
+        respuestaIText.SetText(decision.res_izq.respuesta);
+        respuestaDText.SetText(decision.res_der.respuesta);
+        int anos=anosThreshold*level_index+nAnosIni;
+        anosText.SetText(anos.ToString());
     }
 }
 
