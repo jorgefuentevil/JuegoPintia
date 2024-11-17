dc2142893c586ed8488df42991b3ee7eab8fbaa1
JavSalv <javiersalvadorpenacoba@gmail.com>
2024-11-17 13:12:40 +0100

    Implementada vibración y Limpieza

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,32 +9,64 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject popupLegal;
     [SerializeField] private GameObject textoLegal;
 
-    public void Start(){
+    public void Start()
+    {
         popupLegal.SetActive(false);
         textoLegal.SetActive(true);
+
+        //Carga ajustes de vibracion
+        if (PlayerPrefs.GetInt("VibracionEnabled") == 1)
+        {
+            botonVibracion.GetComponent<Toggle>().isOn = true;
+        }
+        else
+        {
+            botonVibracion.GetComponent<Toggle>().isOn = false;
+        }
     }
 
-    public void Pause(){
+    public void Pause()
+    {
         pauseMenu.SetActive(true);
         mainMenu.SetActive(false);
 
     }
 
-    public void Resume(){
+    public void Resume()
+    {
         pauseMenu.SetActive(false);
         mainMenu.SetActive(true);
     }
 
-    public void ToggleVibracion(){
+    public void ToggleVibracion(bool value)
+    {
 
+        if (value)
+        {
+            Debug.Log("Vibro brrrrrrr");
+            
+            PlayerPrefs.SetInt("VibracionEnabled", 1);
+            #if UNITY_ANDROID || UNITY_IOS
+                Handheld.Vibrate();
+                Debug.Log("Vibro brrrrrrr");
+            #endif
+        }
+        else
+        {
+            PlayerPrefs.SetInt("VibracionEnabled", 0);
+            Debug.Log("No Vibro :(((((");
+
+        }
     }
 
-    public void ShowTerms(){
+    public void ShowTerms()
+    {
         popupLegal.SetActive(true);
         textoLegal.SetActive(false);
     }
 
-    public void HideTerms(){
+    public void HideTerms()
+    {
         popupLegal.SetActive(false);
         textoLegal.SetActive(true);
     }
